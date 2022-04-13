30f033f4340c1f2dd422061a0287f8cec22cda69
LancheMi <milanstojanovic@live.com>
2022-04-13 21:29:03 +0200

    initial test (#1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+    testImplementation("org.junit.platform:junit-platform-suite:1.8.2")
 }
 
 tasks.test {
---
+++ src/test/kotlin/tests/CompareTwoNumbers.kt
@@ -0,0 +1,12 @@
+package tests
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CompareTwoNumbers {
+
+    @Test
+    fun compareTwoNumbers() {
+        assertEquals(1, 2, "numbers are not equal")
+    }
+}
